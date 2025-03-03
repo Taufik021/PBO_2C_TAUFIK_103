1565701161ce627ea96629cc237e711ccbe4c312
Taufik021 <tugastaufik1726@gmail.com>
2025-03-03 16:10:30 +0700

    UPDATE isi file

---
+++ Codeleb/Mencoba/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String nama = "Taufikurhaman";
+        int umur = 20;
+        System.out.println(nama);
+        System.out.print(umur);
+    }
+}
