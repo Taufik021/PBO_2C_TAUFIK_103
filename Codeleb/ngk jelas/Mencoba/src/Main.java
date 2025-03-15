605afb1bc3151534c809314b5af5b38896ca7840
Taufik021 <tugastaufik1726@gmail.com>
2025-03-15 17:06:33 +0700

    Baru

--- Codeleb/ngk jelas/Mencoba/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String nama = "Taufikurhaman";
-        int umur = 20;
-        System.out.println(nama);
-        System.out.print(umur);
-    }
-}
